d893909442e6366cad249a4658716993d622a8f4
luisalbertogila <callmaster200@gmail.com>
2016-12-09 12:58:34 +0100

    Correct table creation on MySQL

--- scripts/upgrade-0.5.4-to-0.5.5_mysql.sql
+++ scripts/upgrade-0.5.4-to-0.5.5_mysql.sql
@@ -6,13 +6,15 @@ ALTER TABLE `accountuser` ADD `pop` int(10) DEFAULT '1' NOT NULL AFTER `imap`;
 ALTER TABLE `accountuser` ADD `sieve` int(10) DEFAULT '1' NOT NULL AFTER `pop`;
 ALTER TABLE `accountuser` ADD `smtpauth` int(10) DEFAULT '1' NOT NULL AFTER `sieve`;
 
-CREATE TABLE `settings` (
-  `username` varchar(50) binary NOT NULL default '',
-  `style` varchar(50) NOT NULL default 'default',
-  `maxdisplay` int(4) NOT NULL default '15',
-  `warnlevel` int(3) NOT NULL default '90',
-  PRIMARY KEY  (username)
-) TYPE=MyISAM;
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `username` varchar(50) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
+  `style` varchar(50) NOT NULL DEFAULT 'default',
+  `maxdisplay` int(4) NOT NULL DEFAULT '15',
+  `warnlevel` int(3) NOT NULL DEFAULT '90'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+ALTER TABLE `settings` ADD PRIMARY KEY (`username`);
 
 INSERT INTO `settings` (username) SELECT `username` FROM adminuser;
 
